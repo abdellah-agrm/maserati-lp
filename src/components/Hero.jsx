27631004421f15520132aecd43ac2fe4ddf6861a
abdellah-agrm <AGOURRAM.ABDELLAH@ofppt-edu.ma>
2024-11-01 17:25:06 +0100

    first commit v3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
   return (
     <div className='container mx-auto min-h-[60vh] flex flex-col justify-end'>
       <h1 className='text-8xl max-md:text-6xl'>{t("hero-1")} <br className='max-md:hidden' /> {t("hero-2")}</h1>
-      <button className='py-2 px-5 border border-white rounded-full bg-none hover:bg-white/15 w-max mt-10 cursor-pointer text-xl max-md:text-lg'>Look</button>
+      <button className='py-2 px-5 border border-white rounded-full bg-none hover:bg-white/15 w-max mt-10 cursor-pointer text-xl max-md:text-lg font-semibold'>Look</button>
     </div>
   )
 }
